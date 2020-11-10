dca1d8c8ce1757ed69f2df28dedcff8daf118fe2
hashicorp-ci <noreply@hashicorp.com>
2020-11-10 20:52:35 +0000

    Release v0.0.0-testingpipeline-5025cf16-514b

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## unreleased
+## 0.0.0-testingpipeline-5025cf16-514b (November 10, 2020)
 
 FEATURES:
 
--- internal/version/version.go
+++ internal/version/version.go
@@ -10,7 +10,7 @@ var (
 	GitCommit   string
 	GitDescribe string
 
-	Version           = "0.1.4"
+	Version           = "0.0.0"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
